347e073cce4cfac4ff1e7888b313091f9cdf99c9
admin <admin@10.20.130.112>
2016-10-10 16:59:58 +0200

    10/10 - PAGE 25
    Affichage de la fenêtre Choice avec l'image de fond

--- Urban Marginal/src/vue/ChoixJoueur.java
+++ Urban Marginal/src/vue/ChoixJoueur.java
@@ -3,11 +3,13 @@ package vue;
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
 
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
 import controleur.Controle;
+import javax.swing.JLabel;
 
 public class ChoixJoueur extends JFrame {
 
@@ -21,11 +23,17 @@ public class ChoixJoueur extends JFrame {
 	public ChoixJoueur(Controle controle) {
 		setTitle("Choice");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 416, 313);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(null);
 		setContentPane(contentPane);
+		
+		JLabel lblLblfond = new JLabel("lblFond");
+		lblLblfond.setBounds(0, 0, 400, 275);
+		
+		ImageIcon image;
+		lblLblfond.setIcon(image = new ImageIcon("media/fonds/fondchoix.jpg"));
+		contentPane.add(lblLblfond);
 	}
-
 } //
