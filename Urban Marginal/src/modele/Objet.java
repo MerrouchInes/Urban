a8dbb45a9b7b695c33322f6f09f2fbe89fca0dc2
admin <admin@10.20.130.112>
2016-10-19 15:44:27 +0200

    19/10 ─ P35 ─ [ Objet ]
    - Getters de la classe Objet. Classe terminée pour l'instant.
    
    - Début de la classe Mur à faire.

---
+++ Urban Marginal/src/modele/Objet.java
@@ -0,0 +1,31 @@
+package modele;
+
+public abstract class Objet {
+	
+	protected int posX ;
+	protected int posY ;
+	protected Label label ;
+	
+	/**
+	 * getPosX
+	 */
+	public int getPosX() {
+		return posX;
+	}
+	
+	/**
+	 * getPosY
+	 */
+	public int getPosY() {
+		return posY;
+	}
+	
+	/**
+	 * getLabel
+	 */
+	public Label getLabel() {
+		return label;
+	}
+	
+
+}
