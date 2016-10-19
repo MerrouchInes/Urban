727191929dbde0ad0d643df01b784e1dec2e6c7f
admin <admin@10.20.130.112>
2016-10-19 15:33:46 +0200

    19/10 ─ P35 ─ [ Label ]
    - Classe Label terminée

--- Urban Marginal/src/modele/Label.java
+++ Urban Marginal/src/modele/Label.java
@@ -1,5 +1,56 @@
 package modele;
 
-public class Label {
+import java.io.Serializable;
 
+import javax.swing.JLabel;
+
+public class Label implements Serializable {
+	
+	private static int nbLabel ;
+	
+	private int numLabel ;
+	private JLabel jLabel ;
+	
+	/**
+	 * Constructeur de la classe Label
+	 */
+	private Label(int numLabel, JLabel jLabel) {
+		this.numLabel = nbLabel ;
+		this.jLabel = jLabel ;
+	}
+	
+	
+	/**
+	 * Getter numLabel
+	 */
+	public int getNumLabel() {
+		return numLabel;
+	}
+	
+	
+	/**
+	 * Getter jLabel
+	 */
+	public JLabel getjLabel() {
+		return jLabel;
+	}
+
+
+	/**
+	 * Getter et Setter de nbLabel
+	 */
+	
+	// ------- Getter -------
+	public static int getNbLabel() {
+		return nbLabel;
+	}
+	
+	// ------- Setter -------
+	public static void setNbLabel(int nbLabel) {
+		Label.nbLabel = nbLabel;
+	}
+	
+	
+	
+	
 }
